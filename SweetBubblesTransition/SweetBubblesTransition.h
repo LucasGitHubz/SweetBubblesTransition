b470493ce9360ec308dbced49275d58ab55b7c72
Lucas <54042430+LucasGitHubz@users.noreply.github.com>
2020-09-16 16:45:54 +0200

    Initial commit

---
+++ SweetBubblesTransition/SweetBubblesTransition.h
@@ -0,0 +1,19 @@
+//
+//  SweetBubblesTransition.h
+//  SweetBubblesTransition
+//
+//  Created by kuroro on 16/09/2020.
+//  Copyright © 2020 lucasam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SweetBubblesTransition.
+FOUNDATION_EXPORT double SweetBubblesTransitionVersionNumber;
+
+//! Project version string for SweetBubblesTransition.
+FOUNDATION_EXPORT const unsigned char SweetBubblesTransitionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SweetBubblesTransition/PublicHeader.h>
+
+
